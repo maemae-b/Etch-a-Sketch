2e0d528f032154483739fe9ed3d8e98458a12d5d
maemae-b <filipinasjessiellemae@gmail.com>
2024-06-20 03:30:59 +0800

    Add operations to make even grid squares and vary color opacity that can be drawn

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ let canvasSize = 16;
 
 function createDivs(sides) {
     
-    for (let i = sides; i > 0; i--)  {
+    for (let i = sides ** 2; i > 0; i--)  {
 
         const NEW_DIV = document.createElement("div");
-        NEW_DIV.addEventListener("mouseenter", () => NEW_DIV.style.backgroundColor = "#1860c6");
+        NEW_DIV.addEventListener("mouseenter", () => {
+            NEW_DIV.style.backgroundColor = "#1860c6"
+            let opacityValue = Number(NEW_DIV.style.opacity);
+            NEW_DIV.style.opacity = `${opacityValue + 0.1}`;});
+        let width = 100/sides;
+        NEW_DIV.style.width = `${width}%`;
+        NEW_DIV.style.opacity = "0.1";
         CONTAINER_DIV.appendChild(NEW_DIV);
     };
 
@@ -20,6 +26,6 @@ const BUTTON = document.querySelector("button.canvas");
 
 BUTTON.addEventListener("click", () => {
     CONTAINER_DIV.textContent = "";
-    canvasSize = Number(prompt("Please enter canvas size: ", 4)) ** 2;
+    canvasSize = Number(prompt("Please enter canvas size: ", 4));
     createDivs(canvasSize);
 });
